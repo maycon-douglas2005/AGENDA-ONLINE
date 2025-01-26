8a9b7f039d4c2b680a00805295845777d842e7d6
maycon-douglas2005 <mayconchaves2005@gmail.com>
2025-01-26 20:58:14 -0300

    feat: adiciona header

---
+++ app/views/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bem Vindo!</title>
+    <link rel="stylesheet" href="../../public/css/index.css">
+    <link rel="stylesheet" href="../../public/css/header.css">
+</head>
+<body>
+    <?php include('partials/header.html') ?>
+    
+    <main>
+        <section class="apresentacao">
+            <div class="container">
+                <div class="textoDeApresentacao">
+                    <h1>SEU DIA A DIA</h1>
+                    <p>de forma mais prática e detalhada!</p>
+                    <hr>
+                    <h2>ORGANIZE AGORA</h2>
+                    <p>sua rotina, utilizando AGENDA ONLINE!</p>
+                </div>
+                <img src="../../public/img/agenda.png" alt="imagem de agenda meramente ilustrativa">
+            </div>
+        </section>
+    </main>
+</body>
+</html>
